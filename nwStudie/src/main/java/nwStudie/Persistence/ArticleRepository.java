1aa7bcf6f591bfd1a944f7c0e7ecc9ec97ba2a1e
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2015-06-30 10:15:06 +0200

    minor changes

--- nwStudie/src/main/java/nwStudie/Persistence/ArticleRepository.java
+++ nwStudie/src/main/java/nwStudie/Persistence/ArticleRepository.java
@@ -14,24 +14,24 @@ import javax.transaction.Transactional;
 public interface ArticleRepository extends CrudRepository<ArticleEntity, String>{
     
 
-    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 1  AND anzahl_woerter > 30 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
+    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 1  AND anzahl_woerter > 50 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
     Iterable<byte[]> findAllArticlesBielefeld();
 
-    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 2  AND anzahl_woerter > 30 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
+    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 2  AND anzahl_woerter > 50 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
     Iterable<byte[]> findAllArticlesPolitik();
 
-    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 3  AND anzahl_woerter > 30 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
+    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 3  AND anzahl_woerter > 50 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
     Iterable<byte[]> findAllArticlesSportBund();
 
-    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 4  AND anzahl_woerter > 30 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
+    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 4  AND anzahl_woerter > 50 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
     Iterable<byte[]> findAllArticlesKultur();
 
-    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 5  AND anzahl_woerter > 30 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
+    @Query(value = "SELECT artikelId FROM artikel WHERE ressortId = 5  AND anzahl_woerter > 50 AND datum = (SELECT max(datum) FROM ausgabe)", nativeQuery = true)
     Iterable<byte[]> findAllArticlesSportBielefeld();
 
 
     //TODO: nur kultur, politik und lokales
-    @Query(value = "SELECT artikelId FROM artikel WHERE  anzahl_woerter > 50 AND datum = (SELECT max(datum) FROM ausgabe) AND (ressortId = 1 OR ressortId = 2 OR ressortId = 4)", nativeQuery = true)
+    @Query(value = "SELECT artikelId FROM artikel WHERE  anzahl_woerter > 70 AND datum = (SELECT max(datum) FROM ausgabe) AND (ressortId = 1 OR ressortId = 2 OR ressortId = 4)", nativeQuery = true)
     Iterable<byte[]> findAllArticles();
 
     ArticleEntity findById(byte[] artikelId);
