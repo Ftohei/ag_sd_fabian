a32bdbcb554d9edcbaa7f4b86b89fb1594ea67ff
Sebastian Walter <swalter@techfak.uni-bielefeld.de>
2015-10-27 14:02:47 +0100

    updated database

--- EsaService/esa.sql
+++ EsaService/esa.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 21. Okt 2015 um 13:38
+-- Erstellungszeit: 27. Okt 2015 um 14:01
 -- Server Version: 5.6.21
 -- PHP-Version: 5.6.3
 
@@ -30,6 +30,7 @@ DROP TABLE IF EXISTS `Artikel`;
 CREATE TABLE IF NOT EXISTS `Artikel` (
   `Id` varbinary(24) NOT NULL,
   `ArtikelID` varchar(255) COLLATE utf8_bin NOT NULL,
+  `ArtikelPDF` varchar(255) COLLATE utf8_bin NOT NULL,
   `Datum` date NOT NULL,
   `Titel` varchar(255) COLLATE utf8_bin NOT NULL,
   `Text` text COLLATE utf8_bin NOT NULL,
@@ -46,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `Artikel` (
 -- Indizes für die Tabelle `Artikel`
 --
 ALTER TABLE `Artikel`
- ADD UNIQUE KEY `Id` (`Id`), ADD UNIQUE KEY `artikelID` (`ArtikelID`), ADD KEY `Datum` (`Datum`);
+ ADD UNIQUE KEY `Id` (`Id`), ADD UNIQUE KEY `artikelID` (`ArtikelID`), ADD KEY `Datum` (`Datum`), ADD KEY `Datum_2` (`Datum`), ADD KEY `Datum_3` (`Datum`), ADD KEY `ArtikelID_2` (`ArtikelID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
